ad83d366fce9bb0351194459e49dfbd479a0fa00
fornetjob <fornetjob@yandex.ru>
2025-04-20 23:23:42 -0500

    added start to package

--- client/server.js
+++ client/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 8080;  // Use the port defined by Azure Web App
+
+// Serve static files from the Angular build (in 'dist' folder)
 app.use(express.static(path.join(__dirname, 'browser')));
 
+// Handle all other routes and serve the Angular app's index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'browser/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+// Start the server
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
